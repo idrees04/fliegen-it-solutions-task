6e61f15d00bffd03bde9c6db42253dc38f3f271d
Muhammad Idrees <midrees0045@gmail.com>
2024-05-24 22:40:26 +0500

    install Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 function App() {
-
-
-  return (   <Container>
-    <h1 className="my-4">Task Tracker</h1>
-  </Container>
-  )
+  return (
+    <Container>
+      <h1 className="my-4">Task Tracker</h1>
+    </Container>
+  );
 }
 
-export default App
+export default App;
