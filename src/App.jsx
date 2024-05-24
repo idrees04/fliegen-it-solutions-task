6565cae806a82a7650007ed8be2e60a86f562ceb
Muhammad Idrees <midrees0045@gmail.com>
2024-05-24 23:08:11 +0500

    add tasks in array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskName) => {
+    const newTask = { id: Date.now(), name: taskName, completed: false };
+    setTasks([newTask, ...tasks]);
+  };
+
   return (
     <Container>
       <h1 className="my-4">Task Tracker</h1>
+      <TaskInput addTask={addTask} />
     </Container>
   );
 }
